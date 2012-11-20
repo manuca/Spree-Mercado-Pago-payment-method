7439cb11e6f18c0fdd0993cfbadb5f2584da4072
Manuel Barros Reyes <manuca@gmail.com>
2012-11-20 19:39:19 -0300

    Reorganize files

--- lib/spree_mercado_pago_payment_method/engine.rb
+++ lib/spree_mercado_pago_payment_method/engine.rb
@@ -5,13 +5,13 @@ module SpreeMercadoPagoPaymentMethod
   class Engine < Rails::Engine
 
     def self.activate
-      # Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
-      #   Rails.application.config.cache_classes ? require(c) : load(c)
-      # end
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
 
-      # Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
-      #   Rails.application.config.cache_classes ? require(c) : load(c)
-      # end
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
     end
 
     initializer "spree_payment_network.register.payment_methods" do |app|
