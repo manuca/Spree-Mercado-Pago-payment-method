7603b9bd8f7a5a81b7b29b822be9532f2b13fd71
Manuel Barros Reyes <manuca@gmail.com>
2012-11-23 01:13:35 -0300

    Controller spec

--- spec/controllers/spree/checkout_controller_spec.rb
+++ spec/controllers/spree/checkout_controller_spec.rb
@@ -1,34 +1,47 @@
 require 'spec_helper'
 
 describe Spree::CheckoutController do
-  let(:order) do
-    mock_model(Spree::Order, :checkout_allowed? => true,
-               :user => nil,
-               :email => nil,
-               :completed? => false,
-               :update_attributes => true,
-               :payment? => false,
-               :insufficient_stock_lines => [],
-               :coupon_code => nil).as_null_object
-  end
+  # let(:order) do
+  #   mock_model(Spree::Order, :checkout_allowed? => true,
+  #              :user => nil,
+  #              :email => nil,
+  #              :completed? => false,
+  #              :update_attributes => true,
+  #              :payment? => false,
+  #              :insufficient_stock_lines => [],
+  #              :coupon_code => nil).as_null_object
+  # end
 
   context "Using Mercado Pago payment method" do
-    # before(:each) do
-    #   payment_method = double("PaymentMethod::MercadoPago")
-    #   Spree::PaymentMethod.stub(:find) { payment_method }
-    #   payment_method.should_receive(:kind_of?) { true }
-    # end
-
-    it "assigns @payment_method of kind PaymentMethod::MercadoPago" do
-      pending
-
+    before(:each) do
       user = double("User", :last_incomplete_spree_order => nil)
       controller.stub(:spree_current_user => user)
-      spree_post :update, {:state => "payment"}
+
+      payment_method = double("PaymentMethod::MercadoPago")
+      payment_method.should_receive(:kind_of?).with(PaymentMethod::MercadoPago) { true }
+      Spree::PaymentMethod.should_receive(:find) { payment_method }
     end
 
-    it "invokes #send_data"
-    it "redirects to m.redirect_url on success"
-    it "renders mercado_pago_error.html.erb on error"
+    it "redirects to m.redirect_url on success" do
+      client = double("client")
+      client.should_receive(:authenticate) { true }
+      client.should_receive(:send_data) { true }
+      client.should_receive(:redirect_url) { "http://www.example.com" }
+      SpreeMercadoPagoClient.should_receive(:new) { client }
+
+      spree_post :update, {:state => "payment"}
+      response.should redirect_to(client.redirect_url)
+    end
+
+    it "renders mercado_pago_error.html.erb on error" do
+      client = double("client")
+      client.should_receive(:authenticate) { false }
+      client.should_receive(:send_data) { true }
+      client.should_receive(:redirect_url) { "http://www.example.com" }
+      SpreeMercadoPagoClient.should_receive(:new) { client }
+
+      spree_post :update, {:state => "payment"}
+      response.should render_template("mercado_pago_error")
+    end
   end
 end
