a42bfab95c6404fef449399b8a8248e984b9f994
Manuel Barros Reyes <manuca@gmail.com>
2012-11-25 18:42:30 -0300

    Ideas for new specs

---
+++ spec/controllers/mercado_pago_controller_sprec.rb
@@ -0,0 +1,23 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe MercadoPagoController do
+  it "finds the order asociated with the current user"
+  it "handles case where user is logged out"
+  # it "doesn't affect current order if there is one (session[:order_id])"
+  
+  describe "#success" do
+    it "marks the order as complete"
+    it "Show success message"
+  end
+
+  describe "#pending" do
+    it "marks order as complete"
+    it "shows pending payment message"
+  end
+
+  describe "#failure" do
+    it "leaves order in payment state"
+    it "shows failure (or cancelled) message"
+  end
+end
