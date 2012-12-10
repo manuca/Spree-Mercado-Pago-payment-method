966d8710ce10fdb7a972334f7d9da3b58ef421d4
Manuel Barros Reyes <manuca@gmail.com>
2012-12-10 01:07:56 -0300

    Tidier routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 Spree::Core::Engine.routes.draw do
-  get "/mercado_pago/success" => "mercado_pago#success", :as => :mercado_pago_success 
-  get "/mercado_pago/pending" => "mercado_pago#pending", :as => :mercado_pago_pending 
-  get "/mercado_pago/failure" => "mercado_pago#failure", :as => :mercado_pago_failure 
+  scope "/mercado_pago", controller: :mercado_pago do
+    get :success, as: :mercado_pago_success 
+    get :pending, as: :mercado_pago_pending 
+    get :failure, as: :mercado_pago_failure 
+  end
 end
