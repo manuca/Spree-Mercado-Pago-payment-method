548cfd972167f338b6b9861c6fa0a711c5e7fe6d
Manuel Barros Reyes <manuca@gmail.com>
2012-12-09 22:56:52 -0300

    New spec that does not pass

--- spec/controllers/spree/mercado_pago_controller_spec.rb
+++ spec/controllers/spree/mercado_pago_controller_spec.rb
@@ -4,11 +4,18 @@ require 'spec_helper'
 describe Spree::MercadoPagoController do
   it "finds the order asociated with the current user"
   it "handles case where user is logged out"
-  # it "doesn't affect current order if there is one (session[:order_id])"
+  it "doesn't affect current order if there is one (session[:order_id])"
   
   describe "#success" do
-    it "marks the order as complete"
-    it "shows success message"
+    let(:user)  { double("User", :last_incomplete_spree_order => nil) }
+    let(:order) { FactoryGirl.create(:order) }
+
+    before { controller.stub(:spree_current_user => user) }
+
+    it "marks the order as complete" do
+      get :success, { order_number: order.number }
+      assigns(:order).state should eq("complete")
+    end
   end
 
   describe "#pending" do
