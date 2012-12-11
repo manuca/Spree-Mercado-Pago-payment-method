9b9614df89f0638c47660c8042ee51fb44208abd
Manuel Barros Reyes <manuca@gmail.com>
2012-12-11 01:11:28 -0300

    Spec

--- spec/controllers/spree/mercado_pago_controller_spec.rb
+++ spec/controllers/spree/mercado_pago_controller_spec.rb
@@ -3,14 +3,23 @@ require 'spec_helper'
 
 describe Spree::MercadoPagoController do
   it "finds the order asociated with the current user"
-  it "handles case where user is logged out"
+  it "handles case where user is logged out redirects to login and back again"
   it "doesn't affect current order if there is one (session[:order_id])"
   
   describe "#success" do
-    let(:user)  { double("User", :last_incomplete_spree_order => nil) }
-    let(:order) { FactoryGirl.create(:order) }
+    let(:user)  { create(:user) }
+    let(:order) do 
+      o = create(:order, user: user, state: "payment") 
+      o.stub(:payment_method) { PaymentMethod::MercadoPago }
+      o
+    end
 
     before { controller.stub(:spree_current_user => user) }
+
+    it "returns success" do
+      spree_get :success, { order_number: order.number }
+      response.should be_success
+    end
 
     it "marks the order as complete" do
       spree_get :success, { order_number: order.number }
