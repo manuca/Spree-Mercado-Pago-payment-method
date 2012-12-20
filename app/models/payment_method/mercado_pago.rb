13c1fc9c75ccb58ecc6f3c827496a534d2b32a46
Manuel Barros Reyes <manuca@gmail.com>
2012-12-20 13:00:17 -0300

    Capture and void working

--- app/models/payment_method/mercado_pago.rb
+++ app/models/payment_method/mercado_pago.rb
@@ -10,28 +10,22 @@ class PaymentMethod::MercadoPago < Spree::PaymentMethod
   def actions
     %w{capture void}
   end
-  
+
   # Indicates whether its possible to capture the payment
   def can_capture?(payment)
     ['checkout', 'pending'].include?(payment.state)
   end
-  
+
   # Indicates whether its possible to void the payment.
   def can_void?(payment)
     payment.state != 'void'
   end
-  
-  def capture(payment)
-    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-    payment.complete
-    payment.order.update!
-    true
+
+  def capture(*args)
+    ActiveMerchant::Billing::Response.new(true, "", {}, {})
   end
-  
-  def void(payment)
-    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-    payment.void
-    payment.order.update!
-    true
+
+  def void(*args)
+    ActiveMerchant::Billing::Response.new(true, "", {}, {})
   end
 end
