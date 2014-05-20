991843a620605e9202a3e81f8692f56a0845adc9
jmperez <jmperez@devartis.com>
2014-05-20 17:03:07 -0300

    Remove unused helpers.
    Conflicts:
    	app/models/mercado_pago/client.rb

--- app/models/mercado_pago/client.rb
+++ app/models/mercado_pago/client.rb
@@ -1,74 +1,69 @@
+# -*- encoding : utf-8 -*-
 require 'rest_client'
 require 'mercado_pago/client/authentication'
 require 'mercado_pago/client/preferences'
 require 'mercado_pago/client/api'
 
 module MercadoPago
-  class Client
-    # These three includes are because of the user of line_item_description from
-    # ProductsHelper
-    include ActionView::Helpers::TextHelper
-    include ActionView::Helpers::SanitizeHelper
-    include Spree::ProductsHelper
 
-    attr_reader :errors
-    attr_reader :auth_response
-    attr_reader :preferences_response
+class Client
+  # These three includes are because of the user of line_item_description from
+  # ProductsHelper
 
-    def initialize(order, callbacks)
-      unless callbacks[:success] && callbacks[:pending] && callbacks[:failure]
-        raise "Url callbacks where not specified"
-      end
+  include Authentication
+  include Preferences
+  include API
 
-      @order = order
-      @callbacks = callbacks
-      @errors = []
-      config_options
-    end
 
-    def authenticate
-      response = send_authentication_request
+  attr_reader :errors
+  attr_reader :auth_response
+  attr_reader :preferences_response
 
-      if response.code != 200
-        @auth_response = nil
-        @errors << I18n.t(:mp_authentication_error) 
-      else
-        @errors = []
-        @auth_response = ActiveSupport::JSON.decode(response)
-      end
+  def initialize(payment_method, options={})
+    @payment_method = payment_method
+    @api_options = options.clone
+    @errors = []
+  end
 
-      @auth_response
-    end
 
-    def send_data
-      response = send_preferences_request
-
-      if response.code != 201
-        @preferences_response = nil
-        @errors << I18n.t(:mp_preferences_setup_error)
-      else
-        @errors = []
-        @preferences_response = ActiveSupport::JSON.decode(response)
-      end
-
-      @preferences_response
-    end
-
-    def redirect_url
-      @preferences_response["init_point"] if @preferences_response.present?
-    end
-
-    private
-
-    def send_authentication_request
-      client_id     = @order.payments.last.payment_method.preferred_client_id
-      client_secret = @order.payments.last.payment_method.preferred_client_secret
-
-      response = RestClient.post(
-        'https://api.mercadolibre.com/oauth/token',
-        {:grant_type => 'client_credentials', :client_id => client_id, :client_secret => client_secret},
-        :content_type=> "application/x-www-form-urlencoded", :accept => 'application/json'
-      )
+  def get_external_reference(mercado_pago_id)
+    response = send_notification_request mercado_pago_id
+    if response
+      response['collection']['external_reference']
     end
   end
+
+  def get_payment_status(external_reference)
+    response = send_search_request({:external_reference => external_reference, :access_token => access_token})
+
+    if response['results'].empty?
+      "pending"
+    else
+      response['results'][0]['collection']['status']
+    end
+  end
+
+  private
+
+
+  def log_error(msg, response, request, result)
+    Rails.logger.info msg
+    Rails.logger.info "response: #{response}."
+    Rails.logger.info "request args: #{request.args}."
+    Rails.logger.info "result #{result}."
+  end
+
+  def send_notification_request(mercado_pago_id)
+    url = create_url(notifications_url(mercado_pago_id), access_token: access_token)
+    options = {:content_type => 'application/x-www-form-urlencoded', :accept => 'application/json'}
+    get(url, options, quiet: true)
+  end
+
+  def send_search_request(params, options={})
+    url = create_url(search_url, params)
+    options = {:content_type => 'application/x-www-form-urlencoded', :accept => 'application/json'}
+    get(url, options)
+  end
+end
+
 end
